0a22dd58208450461a8f710b3efc4e9c1497aa6d
K_Kwon <ts04031@gmail.com>
2022-01-20 13:26:15 +0900

    달력을 통해 날짜를 입력받는 코드 작성 중

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Calculate_Date: Button = findViewById(R.id.btnDatePicker)
+
+        Calculate_Date.setOnClickListener {
+            clickDatePicker()
+        }
+
+    }
+
+    fun clickDatePicker() {
+        Toast.makeText(this,
+            "btnDatePicker pressed", Toast.LENGTH_LONG).show()
+
+        val myCalendar = Calendar.getInstance() // myCalendar는 Calender클래스의 객체
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, day ->  },
+            year,
+            month,
+            day)
     }
 }
