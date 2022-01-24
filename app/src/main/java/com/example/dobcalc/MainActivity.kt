49a2a7de089b42d7884874a2955a80dfb6602e15
K_Kwon <ts04031@gmail.com>
2022-01-25 03:40:16 +0900

    선택한 날짜와 현재까리의 날짜의 분 단위 차이를 출력하고 현재보다 미래의 날짜는 선택할 수 없도록 설정

--- app/src/main/java/com/example/dobcalc/MainActivity.kt
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -12,6 +12,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val Calculate_Date: Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
         Calculate_Date.setOnClickListener {
             clickDatePicker()
@@ -31,21 +33,31 @@ class MainActivity : AppCompatActivity() {
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener { view, selectedyear, selectedmonth, selectedday ->
                 val selectedDate = "$selectedyear/${selectedmonth + 1}/$selectedday"
                 Toast.makeText(this, // month는 0부터 세기 때문에 +1해주어야 함
-                    "Year seleccted $selectedDate", Toast.LENGTH_LONG).show()
+                    "Date seleccted $selectedDate", Toast.LENGTH_LONG).show()
                 tvSelectedDate?.text = selectedDate
 
-                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val sdf = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA)
                 val theDate = sdf.parse(selectedDate) // 위쪽 날짜 형식을 사용하고 싶은 형태로 바꿈
 
+                val selectedDateInMinutes = theDate.time / 60000 // 1970/01/01부터 선택한 날짜까지 시간을 분 단위로
+
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis())) // 1970/01/01부터 현재까지 시간을 ms로
+                val currentDateInMinutes = currentDate.time / 60000
+
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes // 두 날짜 사이의 차이를 분으로 나타냄
+                tvAgeInMinutes?.text = differenceInMinutes.toString()
             },
             year,
             month,
             day
-            ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
+
     }
 }
